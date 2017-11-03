a3bbacd5b21f216ca31d023709a4ef027fcdb3c4
adivjoseph <martin.horne@arm.com>
2017-11-02 22:10:56 -0700

    Update db.inc.php

--- config/db.inc.php
+++ config/db.inc.php
@@ -20,6 +20,9 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+//Remove PHP7.0 warning
+$config = (object)array("db_driver"=>"mysql");
+
  //database driver mysql or pgsql
  $config->db_driver = "mysql";	
 
